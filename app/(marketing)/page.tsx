3050e2740ae5d126bb37b866d0ae2ef952c884f9
thecoderwithHat <vaibhavtheblogger@gmail.com>
2024-11-15 22:55:06 +0530

    wout Meta

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -59,10 +59,10 @@ import faq from '#data/faq'
 import pricing from '#data/pricing'
 import testimonials from '#data/testimonials'
 
-export const meta: Metadata = {
-  title: 'Saas UI Landingspage',
-  description: 'Free SaaS landingspage starter kit',
-}
+// export const meta: Metadata = {
+//   title: 'Saas UI Landingspage',
+//   description: 'Free SaaS landingspage starter kit',
+// }
 
 const Home: NextPage = () => {
   return (
